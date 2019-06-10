bb80679fb541be9148a4cd4d3dd66af326daa27b
Dakara <chad.meadows@gmail.com>
2019-06-10 19:04:46 -0400

    fix template literal expressions

--- src/scope-selectors.ts
+++ src/scope-selectors.ts
@@ -23,18 +23,30 @@ export function makeScopeSelectors<T extends object>(selectors:T, scopeKey = '__
     return selectorsProxy
 }
 
+function standardStringInterpolation(literals:TemplateStringsArray, ...expressions: string[]) {
+    let string = ''
+    for (const [index, value] of expressions.entries()) {
+        string += literals[index] + value
+    }
+
+    string += literals[literals.length - 1]
+    return string
+}
+
 /**
  * Make XPath anchor selector for a link matching link text
  * @param strings 
  */
-export function a(strings:TemplateStringsArray) {
-    return `//a[text()='${strings}']`
+export function a(strings:TemplateStringsArray, ...expressions: string[]) {
+    const string = standardStringInterpolation(strings, ...expressions)
+    return `//a[text()='${string}']`
 }
 
 /**
  * Make XPath element selector for element containing text
  * @param strings 
  */
-export function t(strings:TemplateStringsArray) {
-    return `//*[text()='${strings}']`
+export function t(strings:TemplateStringsArray, ...expressions: string[]) {
+    const string = standardStringInterpolation(strings, ...expressions)
+    return `//*[text()='${string}']`
 }
